2b7d69fd7611aec5240ad83bb156d401d281f2fb
luccaki <lucca.ianaguivara@gmail.com>
2020-09-11 18:22:46 -0300

    Testando movimentação

--- Xadrez console/Program.cs
+++ Xadrez console/Program.cs
@@ -9,11 +9,19 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
-            Tela.imprimirTabuleiro(tab);
+            PartidaDeXadrez partida = new PartidaDeXadrez();
+            while (!partida.terminada)
+            {
+                Console.Clear();
+                Tela.imprimirTabuleiro(partida.tab);
+
+                Console.Write("Origem: ");
+                Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                Console.Write("Destino: ");
+                Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                partida.executaMovimento(origem, destino);
+            }
         }
     }
 }
